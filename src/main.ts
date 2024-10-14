9b0026ef22b7162a5126d7c950fc4936b23b8aa7
Nikotsuki <95511698+Nikotsuki@users.noreply.github.com>
2024-10-14 15:37:41 -0700

    sure

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,8 @@ const app: HTMLDivElement = document.querySelector("#app")!;
 const gameName = "My amazing emoji clikcer game";
 document.title = gameName;
 
-const button = document.createElement('button');
-button.textContent = 'Click isi';
+const button = document.createElement("button");
+button.textContent = "Click isi";
 document.body.appendChild(button);
 
 const header = document.createElement("h1");
