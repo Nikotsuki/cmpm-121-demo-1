e535bb1e31ac5cb53b8c25813a28a798cd791911
Nikotsuki <95511698+Nikotsuki@users.noreply.github.com>
2024-10-16 14:30:37 -0700

    Step 3 Complete

--- src/main.ts
+++ src/main.ts
@@ -24,7 +24,6 @@ button.addEventListener("click", () => {
   counter.innerHTML = count + " flan";
 });
 
-
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
