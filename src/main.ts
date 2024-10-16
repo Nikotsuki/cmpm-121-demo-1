38da71814804a4d7fa16a2cad284538a51584b38
Nikotsuki <95511698+Nikotsuki@users.noreply.github.com>
2024-10-16 14:27:38 -0700

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,6 @@
 import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
-const counter: HTMLDivElement = document.querySelector("#counter")!;
-const purchase: HTMLButtonElement = document.querySelector("#purchase")!;
-//const button: HTMLButtonElement = document.querySelector("#button")!;
-
-let count: number = 0;
-let growth: number = 0;
-
-function increase() {
-  count += growth;
-  counter.innerHTML = count + " flan";
-}
-
-counter.innerHTML = count + " flan";
 
 const gameName = "Flan";
 document.title = gameName;
@@ -21,27 +8,6 @@ document.title = gameName;
 const button = document.createElement("button");
 button.textContent = "🍮";
 document.body.appendChild(button);
-
-button.addEventListener("click", () => {
-  count += 1;
-  counter.innerHTML = count + " flan";
-});
-
-//const purchase = document.createElementById("purchase");
-///purchase.textContent = "Increase Growth Rate";
-//document.body.appendChild(purchase);
-
-while (count >= 10) {
-  purchase.disabled = false;
-}
-
-purchase.addEventListener("click", () => {
-  growth += 1;
-  count -= 10;
-});
-
-setInterval(increase, 1000);
-console.log(count);
 
 //requestAnimationFrame();
 
