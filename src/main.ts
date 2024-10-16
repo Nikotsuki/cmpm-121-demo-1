026219416ef7e218db034adf3cb49399bd2df36e
Nikotsuki <95511698+Nikotsuki@users.noreply.github.com>
2024-10-15 23:11:35 -0700

    counter atempt 1

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import "./style.css";
 
+
 const app: HTMLDivElement = document.querySelector("#app")!;
+const counter: HTMLDivElement = document.querySelector("#counter")!;
+
+let count: number = 0;
+
+
+//const display = document.createElement("counter");
+counter.innerHTML = count + "cookies";
 
 const gameName = "My amazing emoji clicker game";
 document.title = gameName;
@@ -8,6 +16,11 @@ document.title = gameName;
 const button = document.createElement("button");
 button.textContent = "🍮";
 document.body.appendChild(button);
+
+button.addEventListener("click", () => {
+    count += 1;
+});
+
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
