2c5c403d4254bf0a98ab307f5e8e72f1b1d82a91
Nikotsuki <95511698+Nikotsuki@users.noreply.github.com>
2024-10-18 01:59:03 -0700

    ok

--- src/main.ts
+++ src/main.ts
@@ -6,16 +6,16 @@ const growth_rate: HTMLLabelElement = document.querySelector("#Growth")!;
 const button: HTMLButtonElement = document.querySelector("#flan")!;
 
 interface Item {
-  name: string,
-  cost: number,
-  rate: number,
-  bought: number
-};
+  name: string;
+  cost: number;
+  rate: number;
+  bought: number;
+}
 
-const availableItems : Item[] = [
-  {name: "TinyClicker", cost: 10, rate: 0.1, bought: 0},
-  {name: "MamasHelp", cost: 100, rate: 2, bought: 0},
-  {name: "TheSpanishInquisiton", cost: 1000, rate: 50, bought: 0},
+const availableItems: Item[] = [
+  { name: "TinyClicker", cost: 10, rate: 0.1, bought: 0 },
+  { name: "MamasHelp", cost: 100, rate: 2, bought: 0 },
+  { name: "TheSpanishInquisiton", cost: 1000, rate: 50, bought: 0 },
 ];
 
 let count: number = 0;
@@ -29,8 +29,8 @@ function increase() {
   count += growth;
   counter.innerHTML = count + " flan";
   growth_rate.innerHTML = " Growth Rate: " + growth;
-  for (const item of availableItems){
-    if (count >= item.cost){
+  for (const item of availableItems) {
+    if (count >= item.cost) {
       document.getElementById(item.name)?.removeAttribute("disabled");
     }
   }
@@ -39,24 +39,25 @@ function increase() {
 button.addEventListener("click", () => {
   count += 1;
   counter.innerHTML = count + " flan";
-  for (const item of availableItems){
-    if (count >= item.cost){
+  for (const item of availableItems) {
+    if (count >= item.cost) {
       document.getElementById(item.name)?.removeAttribute("disabled");
     }
   }
 });
 
-for (const item of availableItems){
-  document.getElementById(item.name)?.addEventListener("click", () =>{
+for (const item of availableItems) {
+  document.getElementById(item.name)?.addEventListener("click", () => {
     growth += item.rate;
     count -= item.cost;
-    if (count >= item.cost){
+    if (count >= item.cost) {
       document.getElementById(item.name)?.setAttribute("disabled", "disabled");
     }
     item.bought += 1;
     item.cost = item.cost * 1.15;
-    document.getElementById(item.name + "_count")!.innerHTML = " Purchased " + item.name + ": " + item.bought;
-  })
+    document.getElementById(item.name + "_count")!.innerHTML =
+      " Purchased " + item.name + ": " + item.bought;
+  });
 }
 
 setInterval(increase, 1000);
